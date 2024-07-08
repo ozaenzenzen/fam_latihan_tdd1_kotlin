d1bfb36ae29ea2ba9ed28c2bb15fd79e56cec611
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-08 15:14:41 +0700

    first test

---
+++ app/src/test/java/com/dicoding/newsapp/utils/DateFormatterTest.kt
@@ -0,0 +1,13 @@
+package com.dicoding.newsapp.utils
+
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Test
+
+class DateFormatterTest{
+    @Test
+    fun `given correct ISO 8601 format then should format correctly`() {
+        val currentDate = "2022-02-02T10:10:10Z"
+        Assert.assertEquals("02 Feb 2022 | 17:10", DateFormatter.formatDate(currentDate))
+    }
+}
