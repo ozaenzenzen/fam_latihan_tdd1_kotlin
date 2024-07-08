eb8c9becb73aa2707a13bb3c23614564025496c5
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-08 15:17:19 +0700

    desugaring

--- app/src/main/java/com/dicoding/newsapp/utils/DateFormatter.kt
+++ app/src/main/java/com/dicoding/newsapp/utils/DateFormatter.kt
@@ -1,7 +1,18 @@
 package com.dicoding.newsapp.utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.TimeZone
+
 object DateFormatter {
+    @RequiresApi(Build.VERSION_CODES.O)
     fun formatDate(currentDateString: String): String {
-        return "02 Feb 2022 | 17:10"
+        val instant = Instant.parse(currentDateString)
+        val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy | HH:mm")
+            .withZone(ZoneId.of(TimeZone.getDefault().id))
+        return formatter.format(instant)
     }
 }
